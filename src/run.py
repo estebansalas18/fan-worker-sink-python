63762a30883ca091d9d4e078e2b449dbe5ba7c37
Juan Esteban Salas Florez <jesalas@bancolombia.com.co>
2025-11-05 21:55:35 -0500

    feat: agregar logica para leer datos del usuario

--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 from src.fan import generate_numbers, distribute_tasks
 from src.worker import start_workers
 from src.sink import collect_results
+import sys
 
 def main(numbers_count=100, worker_count=3):
 
@@ -15,5 +16,26 @@ def main(numbers_count=100, worker_count=3):
     return final_results
 
 if __name__ == "__main__":
-    results = main()
+    # Leer argumentos desde consola (opcionales)
+    # Ejemplo:
+    #   python run.py 200 5
+    # Si no pasan nada → toma los valores por defecto
+    
+    if len(sys.argv) >= 2:
+        try:
+            numbers_count = int(sys.argv[1])
+        except ValueError:
+            numbers_count = 100
+    else:
+        numbers_count = 100
+
+    if len(sys.argv) >= 3:
+        try:
+            worker_count = int(sys.argv[2])
+        except ValueError:
+            worker_count = 3
+    else:
+        worker_count = 3
+
+    results = main(numbers_count, worker_count)
     print(results)
